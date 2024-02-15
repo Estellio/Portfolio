c2cc6706ed1fef9e42be308bb252e60e62f2cb7c
Esellio <pirko.vichmann@voco.ee>
2024-02-15 18:46:36 +0200

    Finished all Pages layouts

--- rain_bg.js
+++ rain_bg.js
@@ -8,12 +8,12 @@ function rain() {
     while (i < amount) {
         let drop = document.createElement('i');
         drop.className = 'rain';
-        let size = Math.random() * 7;
+        let size = Math.random() * 6;
         let posX = Math.floor(Math.random() * window.innerWidth);
         let delay = Math.random() * -20;
         let duration = Math.random() * 5;
 
-        drop.style.width = 1.5 + size + 'px';
+        drop.style.width = 1.9 + size + 'px';
         drop.style.left = posX + 'px';
         drop.style.animationDelay = delay + 's';
         drop.style.animationDuration = 9 + duration + 's';
